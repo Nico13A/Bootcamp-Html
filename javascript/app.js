c6e3ee06a6edee89a8ba98af55cdc7e8ad89f433
Nicolas Matias Antinao <nicoantinao1998@gmail.com>
2024-02-15 10:00:34 -0300

    Subiendo algo de js.

---
+++ javascript/app.js
@@ -0,0 +1,34 @@
+// CLASE 12
+
+// Creamos una variable
+let nombre = "Nicolas";
+console.log(nombre);
+
+nombre = "Pedro";
+console.log(nombre);
+
+/*
+STRING + STRING = STRING /// "HO" + "LA" = "HOLA"
+STRING + NUMBER = STRING /// "HOLA" + 123 = "HOLA123"
+
+STRING - NUMBER = NUMBER /// "111" - 0 = 111
+*/
+
+let numero = "1";
+console.log(numero + 1 + 1 - 0);//111
+
+let num = 10;
+if (num >= 10) {
+    console.log('Entró');
+}
+
+/*
+NEGACIÓN !
+AND &&
+OR || 
+XOR ^
+XOR (NAND AND OR)
+NAND !(&&)
+NOR !(||)
+XNOR !(^)
+*/
